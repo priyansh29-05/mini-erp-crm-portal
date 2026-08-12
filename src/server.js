fddd73ace2bcceb0ac769002b9fae28ac31028db
priyansh29-05 <priyanshsoni2905@gmail.com>
2026-08-12 11:40:34 +0530

    chore: configure backend start script and CORS for deployment

--- src/server.js
+++ src/server.js
@@ -4,8 +4,22 @@ require('dotenv').config();
 
 const app = express();
 const prisma = new PrismaClient();
+const cors = require('cors');
 
 // Middleware to parse JSON bodies
+const allowedOrigins = ['http://localhost:5173'];
+if (process.env.FRONTEND_URL) {
+  allowedOrigins.push(process.env.FRONTEND_URL);
+}
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 app.use(express.json());
 
 // Routes
